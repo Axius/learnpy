3bfb973f6bc7022caa42f38b399badd3ff0180cf
Axius <florin89z@gmail.com>
2010-08-10 10:37:16 +0300

    new file:   README 	new file:   hello.py 	new file:   relearn.py
    This is the second commit today.

---
+++ hello.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2.6 -tt
+
+#import sys
+
+#def hello(name):
+#    if name == 'Alice':
+#        print 'Alert: Alice Mode'
+#    name = name + '!!!!'
+#    print 'hello', name
+
+#run main():
+#(sys.argv[1])
+#'Hi %s I have %d donuts' % ('Alice', 35)
+#
+#my
+
+a, b = 0, 1
+while b < 10:
+    print b
+    a, b = b, a+b
+
+ def MyAddress():
+     print "Rentea Florin"
+     print "46 Main Street"
+     print "Sibiu, Sadu, Romania"
+     print "k2m 2E9"
+
+ print MyAddress
+
+
---
+++ relearn.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python2.6 -tt
+
+#This a comment to relearn python
+
+#print "hello, world'"
+
+#x = 0.34
+#y = 0.8*4
+#if x == y: 
+#    print "hello"
+#else:
+#    print "this is madness"
+#
+#x = 34
+#y = 37
+
+#if x > y:
+#    print "hello"
+#else:
+#    print "Crazy women"
+    
+#import sys 
+
+#def main():
+#    print sys.argv " Hello, crazy nanny"
+
+
+
+def MyAddress (myName):
+    print myName
+    print "Rentea Florin"
+    print "23 Main Street"
+    print "Sibiu, Sadu, Romania"
+    print "K2M 2E9"
+printMyAddress
+
